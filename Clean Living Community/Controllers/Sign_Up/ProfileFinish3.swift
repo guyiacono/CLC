cad2084540aa25a52456dc4c854e028e3b706962
Michael Karolewicz <s0995867@monmouth.edu>
2018-07-20 16:37:52 -0400

    commit 3rd attempt

--- Clean Living Community/Controllers/Sign_Up/ProfileFinish3.swift
+++ Clean Living Community/Controllers/Sign_Up/ProfileFinish3.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class ProfileFinish3: UIViewController {
+class ProfileFinish3: UIViewController
+{
+    var email: String?
+    var password: String?
+    
+    var fname: String?
+    var lname: String?
+    var dob: String?
+    var home: String?
+    var dor: String?
+    var qAnswer: [Int]?
+    
+    var edu: String?
+    var rel: String?
+    var ori: String?
+    var reli: String?
+    var spt: String?
+    var smk: String?
+    var sup: String?
+    
+    var bio: String?
+    
 
+    @IBOutlet weak var bioSpace: UITextView!
+    
+ 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +48,37 @@ class ProfileFinish3: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBOutlet weak var nextButton: UIButton!
+    @IBAction func nextButtonAction(_ sender: UIButton)
+    {
+        performSegue(withIdentifier: "toProfileFinish4", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if(segue.identifier == "toProfileFinish4")
+        {
+            let destinationVC = segue.destination as! ProfileFinish4
+            destinationVC.qAnswer = qAnswer
+            destinationVC.email = email
+            destinationVC.password = password
+            destinationVC.fname = fname
+            destinationVC.lname = lname
+            destinationVC.dob = dob
+            destinationVC.home = home
+            destinationVC.dor = dor
+            
+            destinationVC.edu = edu
+            destinationVC.rel = rel
+            destinationVC.reli = reli
+            destinationVC.ori = ori
+            destinationVC.spt = spt
+            destinationVC.smk = smk
+            destinationVC.sup = sup
+            
+            destinationVC.bio = bioSpace.text
+        }
+    }
     /*
     // MARK: - Navigation
 
