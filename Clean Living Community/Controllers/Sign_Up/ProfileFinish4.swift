c87e6151b2b30dc56dd322a0390acea6fd694162
Michael Karolewicz <s0995867@monmouth.edu>
2018-07-27 10:49:00 -0400

    Photo selection is working properly?

--- Clean Living Community/Controllers/Sign_Up/ProfileFinish4.swift
+++ Clean Living Community/Controllers/Sign_Up/ProfileFinish4.swift
@@ -49,10 +49,10 @@ class ProfileFinish4: UIViewController, UINavigationControllerDelegate,UIImagePi
         // Do any additional setup after loading the view.
     }
 
-    // @IBAction func changePhoto(_ sender: UIGestureRecognizer)
-    // {
-    //    print("image tapped!")
-    // }
+     @IBAction func changePhoto(_ sender: UIGestureRecognizer)
+     {
+        print("image tapped!")
+     }
  
     @objc func imageTapped(sender: UIImageView)
     {
